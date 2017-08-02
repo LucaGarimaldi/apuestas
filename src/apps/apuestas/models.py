65000ec0a5feedcc594c5c77718c4e96a1cd60be
usuario <usuario@debian>
2017-08-02 18:18:18 -0300

    segundo commit

--- src/apps/apuestas/models.py
+++ src/apps/apuestas/models.py
@@ -4,3 +4,34 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+
+class Preguntas(models.Model):
+    text = models.CharField(max_length=200,null=False,blank=False)
+    due_date = models.DateTimeField("Fecha Limite")
+    create_date = models.DateTimeField("Fecha Publicacion", auto_now_add=True)
+    create_user = models.ForeignKey(User, related_name="pregunta_create_user")
+    update_date = models.DateTimeField("Fecha Actualizacion", auto_now_add=True)
+    update_user = models.ForeignKey(User, related_name="pregunta_update_user")
+
+    def __unicode__(self):
+        return "%s" % (self.pregunta)
+
+    @property
+    def respuestas_validas(self):
+        return RespuestasValidas.objects.filter(pregunta=self)
+
+class RespuestasValidas(models.Model):
+    pregunta = models.ForeignKey(Preguntas, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    create_date = models.DateTimeField("Fecha Publicacion", auto_now_add=True)
+    create_user = models.ForeignKey(User, related_name="pregunta_create_user")
+    update_date = models.DateTimeField("Fecha Actualizacion", auto_now_add=True)
+    update_user = models.ForeignKey(User, related_name="pregunta_update_user")
+
+    class Meta:
+        unique_together = ('pregunta','text')
+
+    def __unicode__(self):
+        return "%s" % (self.pregunta)
