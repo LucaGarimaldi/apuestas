099c3bd60cc70e59bb978904efb97b5828eed332
usuario <usuario@debian>
2017-08-05 12:17:11 -0300

    cuarto commit

--- src/apps/apuestas/views.py
+++ src/apps/apuestas/views.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .forms import PreguntaForm
 
 def index(request):
-    return HttpResponse("Que mas les guste")
+    form = PreguntaForm()
+    if request.method == 'POST':
+        form = PreguntaForm(data=request.POST)
+        if form.is_valid():
+            pregunta = form.save(commit=False)
+            pregunta.create_user = request.user
+            pregunta.update_user = request.user
+            pregunta.save()
+    template = loader.get_template('index.html')
+    context = {
+        'username': request.user.username,
+        'form': form
+    }
+    return HttpResponse(template.render(context, request))
